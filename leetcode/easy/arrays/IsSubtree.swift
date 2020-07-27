f2210ee817187d5bc2970cd2a0caea3dbb01f8e2
Alok Irde <aairde@icloud.com>
2020-07-27 10:44:25 -0700

    migrating to standalone files

--- leetcode/easy/arrays/IsSubtree.swift
+++ leetcode/easy/arrays/IsSubtree.swift
@@ -16,6 +16,19 @@ import Foundation
  * }
  */
 
+public class TreeNode {
+ public var val: Int
+ public var left: TreeNode?
+ public var right: TreeNode?
+ public init() { self.val = 0; self.left = nil; self.right = nil; }
+ public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+     self.val = val
+     self.left = left
+     self.right = right
+ }
+}
+
 extension TreeNode: Equatable, Hashable {
     public static func == (lhs: TreeNode, rhs: TreeNode) -> Bool {
         return lhs.val == rhs.val && lhs.left?.val == rhs.left?.val && lhs.right?.val == rhs.right?.val
